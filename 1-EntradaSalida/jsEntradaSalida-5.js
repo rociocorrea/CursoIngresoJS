523376a21df3d91be6fd8cb84e5f2f1a8479ff8f
Rocio <52494260+rociocorrea@users.noreply.github.com>
2019-07-03 11:57:59 -0300

    Ejercicio 5-Entrada y salida

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,17 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    
+    var nombre ;
+    var edad ; 
+
+    nombre = "José" ;
+    edad = 66 ;
+
+    document.getElementById('elNombre').value = nombre;
+    document.getElementById('laEdad').value = edad;
+
+    alert("Usted se llama " + nombre + " y tiene " + edad + " años") ;
+
 }
 
