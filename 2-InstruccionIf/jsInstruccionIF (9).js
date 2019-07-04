d180ef7b0264fe456ef77935eb52a1127f13fe95
Rocio <52494260+rociocorrea@users.noreply.github.com>
2019-07-04 12:03:18 -0300

    Ejercicio 9

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,9 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+	//Se usa este método para mostrar enteros de manera aleatoria
+
+	alert(Math.floor(Math.random() * 11)) ;
 
 }//FIN DE LA FUNCIÓN
