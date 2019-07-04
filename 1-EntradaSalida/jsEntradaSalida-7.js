fd9d49035808f7232a06a531fc9606dd970099d9
Rocio <52494260+rociocorrea@users.noreply.github.com>
2019-07-04 09:28:21 -0300

    Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,60 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+    var numeroUno ;
+    var numeroDos ;
+    var suma ;
+
+    numeroUno = document.getElementById('numeroUno').value ;
+    numeroDos = document.getElementById('numeroDos').value ;
+
+    suma = parseInt(numeroUno) + parseInt(numeroDos) ;
+
+    alert("El resultado de la suma es : " + suma) ;
 	
 }
 
 function restar()
 {
-	
+	var numeroUno ;
+    var numeroDos ;
+    var resta ;
+
+    numeroUno = document.getElementById('numeroUno').value ;
+    numeroDos = document.getElementById('numeroDos').value ;
+
+    resta = parseInt(numeroUno) - parseInt(numeroDos) ;
+
+    alert("El resultado de la resta es : " + resta) ;
+
+    
 }
 
 function multiplicar()
 { 
-	
+	var numeroUno ;
+    var numeroDos ;
+    var multiplicacion ;
+
+    numeroUno = document.getElementById('numeroUno').value ;
+    numeroDos = document.getElementById('numeroDos').value ;
+
+    multiplicacion = parseInt(numeroUno) * parseInt(numeroDos) ;
+    
+    alert("El resultado de la multiplicación es : " + multiplicacion) ;
 }
 
 function dividir()
 {
-	
+	var numeroUno ;
+    var numeroDos ;
+    var division ;
+
+    numeroUno = document.getElementById('numeroUno').value ;
+    numeroDos = document.getElementById('numeroDos').value ;
+
+    division = parseInt(numeroUno) / parseInt(numeroDos) ;
+    
+    alert("El resultado de la división es : " + division) ;
 }
 
