9831b738fbe6ab26262ec2e930fb6782a1f1e8fb
Rocio <52494260+rociocorrea@users.noreply.github.com>
2019-07-04 11:28:36 -0300

    Ejercicios del 1 al 5

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,18 @@
 function mostrar()
-{
-//tomo la edad  
+{//tomo la edad
 
+    var edad ;
+
+    edad = document.getElementById('edad').value ;
+
+    /*NO ME TOMA if((edad < 13) && (edad > 17)) {
+        alert(Usted NO es adolescente!) ;
+    }*/
+    if(edad < 13){
+        alert("Usted NO es adolescente!") ;
+
+    }else if(edad > 17){
+        alert("Usted NO es adolescente!") ;
+    }
 
 }//FIN DE LA FUNCIÓN
