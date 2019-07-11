f306bc118639910925111e81cd7c3d409c919096
rocio <hola>
2019-07-11 11:10:11 -0300

    Ejercicio 5 con ejemplo

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,18 +1,28 @@
 function mostrar()
 {//tomo la edad
 
+    var precio ;
+
+    precio = document.getElementById('edad').value ;
+
+    if(precio < 25){
+        alert("BARATO!") ;
+    }else{
+        if(precio > 75){
+            alert("CARO!") ;
+        }else {
+            alert("BIEN!");
+        }
+    }
+    /*
     var edad ;
 
     edad = document.getElementById('edad').value ;
 
-    /*NO ME TOMA if((edad < 13) && (edad > 17)) {
-        alert(Usted NO es adolescente!) ;
-    }*/
-    if(edad < 13){
-        alert("Usted NO es adolescente!") ;
-
-    }else if(edad > 17){
+    if(edad > 18){
         alert("Usted NO es adolescente!") ;
     }
+
+    */
 
 }//FIN DE LA FUNCIÓN
