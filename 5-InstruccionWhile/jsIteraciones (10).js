5c74563c1507e5db0c18aa2be0388e217e3f111c
alumno <alumno@utnfra.com.ar>
2019-07-18 11:37:20 -0300

    ejercicios while

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,35 @@
 function mostrar()
 {
-
+	var numero;
 	var contador=0;
+	var negativos=0;
+	var positivos= 0;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
 
 	while(respuesta!="no")
 	{
+		contador++;
+		numero=prompt("Ingresar un número");
+		numero=parseInt(numero);
+
+		while(isNaN(numero))
+		{
+			numero=prompt("ERROR, ingrese número nuevamente");
+		}
+
+		respuesta=prompt("Quiere seguir agregando números?");
+
+		if(numero < 0)
+		{
+			resultado= (negativos += numero);
+		}
 		
 	
 	}
 
-
+	document.write(resultado);
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,6 +2,8 @@ function mostrar()
 {
 	var numero;
 	var contador=0;
+	var maximo;
+	var minimo;
 	// declarar variables
 	
 	var respuesta='si';
@@ -17,12 +19,38 @@ function mostrar()
 			numero=prompt("ERROR, ingresar un NÚMERO");
 		}
 
-		Math.max(numero);
-		Math.min(numero);
-		
+		respuesta=prompt("Quiere seguir agregando números?");
+
+		if(contador == 1)
+		{
+			minimo = numero;
+			maximo = numero;
+
+
+	//ESTAN MAL LAS CONDICIONES, ME TIRA SOLO EL PRIMER NUM QUE INGRESO
+			if(numero < minimo)
+			{
+				numero = minimo;
+
+			}else if(numero > maximo)
+			{
+				maximo = minimo;
+			}
+		}
+		/*; minimo y maximo 
+		maximo 0 number
+		minimo = num
+		*/
+
+		document.getElementById('maximo').value= maximo;
+		document.getElementById('minimo').value= minimo;
+		/* numero<minimo
+		minimo = mni
+		num mayor a max
+		max=min
+		*/
+
+
 	}
-
-
-
 
 }//FIN DE LA FUNCIÓN
