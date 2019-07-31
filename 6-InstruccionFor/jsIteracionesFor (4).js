00b5c6ea84ee217b9b69707ec563f2aaefa00607
alumno <alumno@utnfra.com.ar>
2019-07-31 11:28:43 -0300

    for

--- 6-InstruccionFor/jsIteracionesFor (4).js
+++ 6-InstruccionFor/jsIteracionesFor (4).js
@@ -1,5 +1,15 @@
 function mostrar()
 {
+    var infinito;
+
+    for(var i=0;i<1000000;i++){
+        
+        infinito=prompt(i);
+
+        if(infinito == "BREAK"){
+            break;
+        }
+    }
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,5 +1,15 @@
 function mostrar()
 {
+    var infinito;
+
+    for(var i=0;i<1000000;i++){
+        
+        infinito=prompt(i);
+
+        if(infinito=="9"){
+            break;
+        }
+    }
 
 
 
