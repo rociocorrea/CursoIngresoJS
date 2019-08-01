09e67f02aa962bebbbf884dfb3a1f17aeef438ff
alumno <alumno@utnfra.com.ar>
2019-08-01 09:29:24 -0300

    for

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,12 +1,25 @@
 function mostrar()
 {
     var numero;
+    var pares;
     
     numero=prompt("Ingrese un número: ");
-    
 
-    for(var i=1;i<numero;i++){
+    while(isNaN(numero)){
+        numero=prompt("ERROR");
+        numero=parseInt(numero);
+    }
 
+    for(var i=1;i<=numero;i++){
+        
+        pares=i%2;
+
+        if(pares==0){
+            document.write( i + "<br>");
+        }
 
     }
+
 }//FIN DE LA FUNCIÓN
+//numero modulo dos y resulta 0, el numero es par
+//numero divisible por i = 0
